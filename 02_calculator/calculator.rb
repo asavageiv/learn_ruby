2b7220f6af4989043fb2dad7f0f4d381176f557f
Alan Savage <asavageiv@gmail.com>
2020-04-08 16:22:03 -0400

    02_calculator solution

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,34 @@
-#write your code here
+# frozen_string_literal: true
+
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(arr)
+  arr.reduce(0, :+)
+end
+
+def multiply(arr)
+  arr.reduce(1, :*)
+end
+
+def power(base, exponent)
+  base**exponent
+end
+
+def factorial(n)
+  if n.zero?
+    1
+  else
+    result = 1
+    n.times do |i|
+      result *= (i + 1)
+      puts "#{result} #{i + 1}"
+    end
+    result
+  end
+end
