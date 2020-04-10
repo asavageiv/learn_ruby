8fda0dcd855bc4edc3302009edfad02352a9249c
Alan Savage <asavageiv@gmail.com>
2020-04-10 16:23:33 -0400

    06_time solution

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,26 @@
+# frozen_string_literal: true
+
+# Timer takes a time in its seconds attribute and prints it in the format
+# hh:mm:ss
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def padded(val)
+    if val.to_s.length < 2
+      "0#{val}"
+    else
+      val.to_s
+    end
+  end
+
+  def time_string
+    hours = seconds / 3600
+    minutes = (seconds % 3600) / 60
+    seconds = (self.seconds % 3600 % 60)
+    "#{padded(hours)}:#{padded(minutes)}:#{padded(seconds)}"
+  end
 end
