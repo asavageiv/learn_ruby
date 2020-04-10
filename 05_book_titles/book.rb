e5add6e46e66a457868fc0609c1e3ea23238bb43
Alan Savage <asavageiv@gmail.com>
2020-04-10 16:01:45 -0400

    05_book_titles solution

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
+# frozen_string_literal: true
+
+LITTLE_WORDS = %w[a an the and but or for nor over under around on in of].freeze
+def titleize(msg)
+  words = msg.split(' ').map do |word|
+    LITTLE_WORDS.include?(word.downcase) ? word : word.capitalize
+  end
+  words[0] = words[0].capitalize
+  words.join(' ')
+end
+
+# Book represents a book
 class Book
-# write your code here
+  attr_reader :title
+
+  def title=(title)
+    @title = titleize(title)
+  end
 end
